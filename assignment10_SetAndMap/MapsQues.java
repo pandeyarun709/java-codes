3a1bc3b550df3949a994acc026f6253391a75e6b
pandeyarun709 <pandeyarun709@gmail.com>
2018-06-21 18:01:57 +0530

    Assignmet 11

---
+++ assignment10_SetAndMap/MapsQues.java
@@ -0,0 +1,19 @@
+package assignment10_SetAndMap;
+
+import java.util.HashMap;
+
+public class MapsQues {
+	
+	
+	public static void main(String[] args) {
+        HashMap<String,Integer> firstMap = new HashMap<>();
+        firstMap.put("Arun", 1186);
+        firstMap.put("Ayush", 1201);
+        firstMap.put("Aryan", 1191);
+       
+        System.out.print("Content in Map : ");
+        System.out.println(firstMap);
+        System.out.println("Value corresponding to Arun  is : " + firstMap.get("Arun"));
+    }
+
+}
